63d9423648a25fc4700c9014647e967d78a0437b
Jessica <jessicacristina.recife@gmail.com>
2023-09-14 21:37:53 -0300

    exer casa

--- exercicios/para-casa/exerciciocasa.py
+++ exercicios/para-casa/exerciciocasa.py
@@ -21,3 +21,37 @@ Crie instâncias de Carro e Moto, atribuindo valores adequados aos atributos de 
 
 Calcule e imprima o imposto a ser pago por cada veículo usando o método calcular_imposto().
 '''
+
+class Veiculo:
+    def __init__(self, modelo, ano, preco):
+        self.modelo = modelo
+        self.ano = ano
+        self.preco = preco
+
+    def calcular_imposto(self):
+        return 0.10 * self.preco
+
+class Carro(Veiculo):
+    def __init__(self, modelo, ano, preco, marca):
+        super().__init__(modelo, ano, preco)
+        self.marca = marca
+
+    def desconto(self):
+        return 0.05 * self.preco
+
+class Moto(Veiculo):
+    def __init__(self, modelo, ano, preco, cilindrada):
+        super().__init__(modelo, ano, preco)
+        self.cilindrada = cilindrada
+
+    def calcular_imposto(self):
+        return 0.05 * self.preco
+
+carro1 = Carro("BMW iX3", 2022, 130000.00, "BMW")
+moto1 = Moto("Scooter", 2023, 6000.00, 600)
+
+imposto_carro = carro1.calcular_imposto()
+imposto_moto = moto1.calcular_imposto()
+
+print(f"Imposto a ser pago pelo carro: R${imposto_carro:.2f}")
+print(f"Imposto a ser pago pela moto: R${imposto_moto:.2f}")
